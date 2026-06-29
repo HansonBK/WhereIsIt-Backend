d947a259f1ef9053f916aa1d87c7443980b5a8ad
Hussein Bani Khaled <hansonhans88@gmail.com>
2026-06-29 15:42:11 -0400

    creating all properites funcions

--- src/services/property.service.js
+++ src/services/property.service.js
@@ -23,7 +23,66 @@ async function createProperty(name, userId) {
     }
 }
 
+async function getAllProperties() {
+    
+    try {
+
+        return await prisma.property.findMany();
+        
+    } catch (error) {
+        console.error("Error fetching properties!: ", error);
+        throw error;
+        
+    }
+}
+
+async function getpropertyById(propertyId) {
+    try {
+        return await prisma.property.findUnique({
+            where:{
+                id : propertyId
+            }
+        }) 
+
+    } catch (error) {
+        console.error("Error fetching property: ", error);
+        throw error;
+    }
+}
+
+async function updateProperty(propertyId, updateData) {
+    
+    try {
+        return await prisma.property.update({
+            where:{
+                id: userId
+            },
+            data: updateData
+        })
+    } catch (error) {
+        console.error("Error updating property: ", error);
+        throw error;
+    }
+}
+
+async function deleteProperty(propertyId) {
+
+    try {
+        return await prisma.property.delete({
+            where:{
+                id: userId
+            }
+        })
+    } catch (error) {
+        console.error("Error deleting property: ", error);
+        throw error;
+    }
+}
+
 module.exports = {
     createProperty,
- 
+    getAllProperties,
+    getpropertyById,
+    updateProperty,
+    deleteProperty
 };
